b414ee5dcb9aa9dc7f8d4a01bad6187c327e38b8
Jay <jay.mm.message@gmail.com>
2020-01-04 20:32:35 +0800

    利用 dictionary 來紀錄 vowels 出現的次數

---
+++ ch01/vowels_dictionary.py
@@ -0,0 +1,27 @@
+
+topic = 'Provide a word to search for vowels: '
+
+vowels = [ 'a', 'e', 'i', 'o', 'u' ]
+
+word = input(topic)
+
+found = {}
+found['a'] = 0
+found['e'] = 0
+found['i'] = 0
+found['o'] = 0
+found['u'] = 0
+
+print('\n', 'Before search for vowels: ')
+for key, value in sorted(found.items()):
+    print('\t', key, 'was found', value, 'time(s).')
+
+for letter in word:
+    if letter in vowels:
+        found[letter] += 1
+
+print('\n', 'After search for vowels: ')
+for key, value in sorted(found.items()):
+    print('\t', key, 'was found', value, 'time(s).')
+
+print()
