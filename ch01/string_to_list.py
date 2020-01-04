a068f6f343389a2b4c1e0f23eb0ffa88793514f5
Jay <jay.mm.message@gmail.com>
2020-01-04 18:38:53 +0800

    字串和串列互相轉換

---
+++ ch01/string_to_list.py
@@ -0,0 +1,9 @@
+string = "Don't panic"
+string_to_list = list(string)
+
+print("string: ", string)
+print("string to list: ", string_to_list)
+
+list_to_string = ''.join(string_to_list)
+print("list to string: ", list_to_string)
+
