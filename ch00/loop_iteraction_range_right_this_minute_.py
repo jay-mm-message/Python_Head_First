83804d75a71d2b97297faea4be75876a2375764b
Jay <jay.mm.message@gmail.com>
2020-01-05 14:03:56 +0800

    利用 for 迴圈迭代和 range(5) 執行 5 次, right_this_minute 判斷

---
+++ ch00/loop_iteraction_range_right_this_minute_.py
@@ -0,0 +1,15 @@
+
+
+from datetime import datetime
+
+odd = range(1, 60, 2)
+print(list(odd))
+
+right_this_minute = datetime.today().minute
+
+for num in range(5):
+    print(right_this_minute)
+    if right_this_minute in odd:
+        print('This minute seems a little odd.')
+    else:
+        print('Not an odd minute')
