811110fb48a0d7fe4ed5012a3d3a5596a835df73
Jay <jay.mm.message@gmail.com>
2020-01-05 13:20:47 +0800

    透過 if 條件判斷, 來確認 right_this_minute, 值有存在 odds ( 1 ~ 60 ) 基數串列內.

---
+++ ch00/if_statement_.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
+        21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+        41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
+
+
+# 當 Today 函式被呼叫, 會回傳一個時間物件, 這時間物件包含很多屬性, 在python可以透過 '.' 點號 ( dot-notation ) 取得
+# 特定需要的屬性 ( attribute )
+right_this_minue = datetime.today().minute
+
+if right_this_minue in odds:
+    print("right_this_minue : ", right_this_minue, ", 值有存在 odds ( 1 ~ 60 ) 基數串列內.")
+else:
+    print("right_this_minue : ", right_this_minue, ", 值有不存在 odds ( 1 ~ 60 ) 基數串列內.")
+
+print()
