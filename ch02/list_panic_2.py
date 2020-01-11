992e7707e86e7e99725d6b470d732c98c58a2abc
Jay <jay.mm.message@gmail.com>
2020-01-11 16:30:28 +0800

    將 list operation ( extend, remove, pop, insert) 用 slice 替代

--- ch02/list_panic_2.py
+++ ch02/list_panic_2.py
@@ -5,17 +5,19 @@ plist = list(phrase)
 print('phrase: ', phrase)
 print('plist: ', plist)
 
+# list ( slice ) 因為可以個別一筆一筆讀取所以不會破壞 plise 的結構
 new_phrase = ''.join(plist[1:3])
 new_phrase = new_phrase + ''.join([plist[5], plist[4], plist[7], plist[6]])
 
-#for i in range(4):
+# list ( pop, remove, extend, insert.) 操作在 plist.extend([plist.pop(), plist.pop()]) 會破壞 plist 的結構
+# for i in range(4):
 #    plist.pop()
 
-#plist.pop(0)
-#plist.remove("'")
-#plist.extend([plist.pop(), plist.pop()])
-#plist.insert(2, plist.pop(3))
-#new_phrase = ''.join(plist)
+# plist.pop(0)
+# plist.remove("'")
+# plist.extend([plist.pop(), plist.pop()])
+# plist.insert(2, plist.pop(3))
+# new_phrase = ''.join(plist)
 
 print('plist: ', plist)
 print('new_phrase: ', new_phrase)
