bc183bbf0801db57741ea1ea4a3fc3a77e4a9353
Jay <jay.mm.message@gmail.com>
2020-01-11 15:08:30 +0800

    使用 slice 作到字串反轉

--- ch02/list_slice_reverse.py
+++ ch02/list_slice_reverse.py
@@ -8,5 +8,8 @@ print('list_word : ', list_word)
 reverse_list_word = list_word[::-1]
 print('reverse_list_word : ', reverse_list_word)
 
+reverse_list_word_2 = list_word[-1:-len(list_word)-1:-1]
+print('reverse_list_word_2 : ', reverse_list_word_2)
+
 reverse_word = ''.join(reverse_list_word)
 print('reverse_word : ', reverse_word)
