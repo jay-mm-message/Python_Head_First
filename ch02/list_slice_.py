4446183bdd510dd2d78506e09a3d63638c78d6bb
Jay <jay.mm.message@gmail.com>
2020-01-11 14:19:23 +0800

    實做 python slice 用法

---
+++ ch02/list_slice_.py
@@ -0,0 +1,10 @@
+
+word = '12345'
+list_word = list(word)
+
+print('slice, [start:stop:step]')
+print('list_word : ', list_word)
+print('list_word[3:] : ', list_word[3:])
+print('list_word[:4] : ', list_word[:4])
+print('list_word[::2] : ', list_word[::2])
+print('list_word[0:5:1] : ', list_word[0:5:1])
