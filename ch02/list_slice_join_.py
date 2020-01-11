b9447d60792f69e21a15e08ca3057929bb79168f
Jay <jay.mm.message@gmail.com>
2020-01-11 15:01:14 +0800

    這可以看到當 ' 引號在轉成 list 時候會帶 雙引號, slice -6 是從後面由 -1 開始算第六個.

---
+++ ch02/list_slice_join_.py
@@ -0,0 +1,8 @@
+boot = 'The Hitchhiker\'s Guide to the Galaxy'
+print('boot : ', boot)
+
+list_boot = list(boot)
+print('list_boot', list_boot)
+
+word = ''.join(list_boot[-6:])
+print('\'\'.join(list_boot[-6:]', word)
