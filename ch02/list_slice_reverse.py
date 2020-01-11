dfb21586932c2f22ec3f594afd886ff6805b2b1c
Jay <jay.mm.message@gmail.com>
2020-01-11 15:08:30 +0800

    使用 slice 作到字串反轉

---
+++ ch02/list_slice_reverse.py
@@ -0,0 +1,12 @@
+
+word = '12345'
+print('word : ', word)
+
+list_word = list(word)
+print('list_word : ', list_word)
+
+reverse_list_word = list_word[::-1]
+print('reverse_list_word : ', reverse_list_word)
+
+reverse_word = ''.join(reverse_list_word)
+print('reverse_word : ', reverse_word)
