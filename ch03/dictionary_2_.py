c476b04b48a1637935ce36bbe6bdf9eeb424b9fa
Jay <jay.mm.message@gmail.com>
2020-01-14 22:33:28 +0800

    新增 python 結構化資料  dictionary . 新增 結構化資料 加入資料方式

---
+++ ch03/dictionary_2_.py
@@ -0,0 +1,10 @@
+
+person_3 = {'Name':'Ford Prefect', 'Gander':'Male', 'Occupation':'Researcher', 'Home Planet':'Betelgeuse Seven'}
+
+print('person_3: ', person_3)
+
+print('person_3: add[ Max:123 ]')
+
+person_3['Max'] = 123
+
+print('persion_3: ', person_3)
